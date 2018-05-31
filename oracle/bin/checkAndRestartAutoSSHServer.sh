496760bb94cb8ced946fca2513dc7d988904a13e
John Graves <john.graves@oracle.com>
2018-05-31 14:36:32 +1000

    Updated register script for VM

--- oracle/bin/checkAndRestartAutoSSHServer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-numberRunning=`ps -ef | grep autossh | grep -v checkAndRestartAutoSSHServer.sh | grep -v grep | wc -l`
-
-if [ $numberRunning -lt 1 ]; then
-  /bin/date >> /home/oracle/reverseTunnels.log
-  /home/oracle/bin/setupReverseSSHPorts.sh >> /home/oracle/reverseTunnels.log&
-fi
